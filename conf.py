eee9838188e55ab1da7d8cc585aab87217f0d7a4
Rajadurgaramdas <53502232+Rajadurgaramdas@users.noreply.github.com>
2021-02-23 16:45:02 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git & github project'
+copyright = '2021, ram'
+author = 'ram'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
